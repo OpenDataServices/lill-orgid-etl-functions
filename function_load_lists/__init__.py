861cffba50c9d18458356381dd2aa692d8236b1c
James B <james.baster@opendataservices.coop>
2023-03-16 08:48:24 +0000

    Azure Functions - switch from psql to solr

--- function_load_lists/__init__.py
+++ function_load_lists/__init__.py
@@ -1,9 +1,9 @@
 import lillorgid.etl.logging
-import lillorgid.etl.l.psql.lib
+import lillorgid.etl.l.solr.lib
 
 lillorgid.etl.logging.log_to_azure()
 
 def main(context):
     lillorgid.etl.logging.logger.info("Function function_load_lists called")
-    lillorgid.etl.l.psql.lib.load_lists()
+    lillorgid.etl.l.solr.lib.load_lists()
     return "function_load_lists done"
--- function_load_threesixtygiving/__init__.py
+++ function_load_threesixtygiving/__init__.py
@@ -1,5 +1,5 @@
 import lillorgid.etl.logging
-import lillorgid.etl.l.psql.lib
+import lillorgid.etl.l.solr.lib
 
 lillorgid.etl.logging.log_to_azure()
 
@@ -7,9 +7,9 @@ lillorgid.etl.logging.log_to_azure()
 def main(context):
     lillorgid.etl.logging.logger.info("Function function_load_threesixtygiving called")
 
-    worker = lillorgid.etl.l.psql.lib.Runner(
-        lillorgid.etl.l.psql.lib.Reader(),
-        lillorgid.etl.l.psql.lib.Writer()
+    worker = lillorgid.etl.l.solr.lib.Runner(
+        lillorgid.etl.l.solr.lib.Reader(),
+        lillorgid.etl.l.solr.lib.Writer()
     )
     worker.go_data_standard('threesixtygiving')
 
