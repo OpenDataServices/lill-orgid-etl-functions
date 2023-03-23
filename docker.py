0b9a4c3662b8b2605d8b608dca548be3a3e373f7
James B <james.baster@opendataservices.coop>
2023-03-23 08:57:21 +0000

    Docker - keep going if errors

--- docker.py
+++ docker.py
@@ -8,30 +8,47 @@ import tempfile
 
 lillorgid.etl.logging.python_logging_to_stdout()
 
-lillorgid.etl.logging.logger.info("Docker - Load Lists!")
-lillorgid.etl.l.psql.lib.load_lists()
+try:
+    lillorgid.etl.logging.logger.info("Docker - Load Lists!")
+    lillorgid.etl.l.psql.lib.load_lists()
+except Exception as e:
+    lillorgid.etl.logging.logger.info("ERROR - " + str(e))
 
-lillorgid.etl.logging.logger.info("Docker - Code4IATI Data Dump!")
-worker = lillorgid.etl.et.iati.datadump_code4iati.IATIDataDump(tempfile.mkdtemp(prefix="lillorgiddatadump_code4iati"))
-worker.download_data()
-worker.extract_zip()
-worker.extract_transform()
-del worker
+try:
+    lillorgid.etl.logging.logger.info("Docker - Code4IATI Data Dump!")
+    worker = lillorgid.etl.et.iati.datadump_code4iati.IATIDataDump(tempfile.mkdtemp(prefix="lillorgiddatadump_code4iati"))
+    worker.download_data()
+    worker.extract_zip()
+    worker.extract_transform()
+    del worker
+except Exception as e:
+    lillorgid.etl.logging.logger.info("ERROR - " + str(e))
 
-lillorgid.etl.logging.logger.info("Docker - INDIGO!")
-worker = lillorgid.etl.et.indigo.scraper.INDIGOScraper(tempfile.mkdtemp(prefix="lillorgidindigo"))
-worker.go()
-del worker
 
-lillorgid.etl.logging.logger.info("Docker - Three Sixty Giving!")
-worker = lillorgid.etl.et.threesixtygiving.datastore.ThreeSixtyGivingDataStore(tempfile.mkdtemp(prefix="lillorgidthreesixtygiving"))
-worker.go()
-del worker
+try:
+    lillorgid.etl.logging.logger.info("Docker - INDIGO!")
+    worker = lillorgid.etl.et.indigo.scraper.INDIGOScraper(tempfile.mkdtemp(prefix="lillorgidindigo"))
+    worker.go()
+    del worker
+except Exception as e:
+    lillorgid.etl.logging.logger.info("ERROR - " + str(e))
 
-lillorgid.etl.logging.logger.info("Docker - Load all data!")
-worker = lillorgid.etl.l.solr.lib.Runner(
-    lillorgid.etl.l.solr.lib.Reader(),
-    lillorgid.etl.l.solr.lib.Writer()
-)
-worker.go()
-del worker
+try:
+    lillorgid.etl.logging.logger.info("Docker - Three Sixty Giving!")
+    worker = lillorgid.etl.et.threesixtygiving.datastore.ThreeSixtyGivingDataStore(tempfile.mkdtemp(prefix="lillorgidthreesixtygiving"))
+    worker.go()
+    del worker
+except Exception as e:
+    lillorgid.etl.logging.logger.info("ERROR - " + str(e))
+
+try:
+    lillorgid.etl.logging.logger.info("Docker - Load all data!")
+    worker = lillorgid.etl.l.solr.lib.Runner(
+        lillorgid.etl.l.solr.lib.Reader(),
+        lillorgid.etl.l.solr.lib.Writer()
+    )
+    worker.go()
+    del worker
+except Exception as e:
+    lillorgid.etl.logging.logger.info("ERROR - " + str(e))
+
